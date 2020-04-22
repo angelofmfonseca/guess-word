61dec130b1d518fb2f6ba3ef4b9c6477590fc8de
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-04-21 23:33:51 -0300

    congrats component and green tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export default function Congrats() {
-  return (
-    <section className="Congrats" data-test="congrats-component"></section>
-  );
+export default function Congrats(props) {
+  if (props.success) {
+    return (
+      <section className="Congrats" data-test="congrats-component">
+        <span data-test="congrats-message">
+          Congratilations!!! You gessed the word!!!
+        </span>
+      </section>
+    );
+  }
+  return <section data-test="congrats-component" />;
 }
