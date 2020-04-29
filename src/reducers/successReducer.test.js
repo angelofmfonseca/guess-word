738f6fba1e7fafa2a8c514f76685ad335177e52a
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-04-28 22:44:10 -0300

    successReducer

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -2,7 +2,7 @@ import { actionTypes } from "../actions/index";
 import successReducer from "./successReducer";
 
 it("returns default initial state of false when no action is passed", () => {
-  const newState = successReducer();
+  const newState = successReducer(undefined, {});
   expect(newState).toBe(false);
 });
 
