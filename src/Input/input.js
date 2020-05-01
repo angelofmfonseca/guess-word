5a39066222a317230fd085e18336f153b7263acf
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-01 20:02:16 -0300

    input form

--- src/Input/input.js
+++ src/Input/input.js
@@ -5,12 +5,19 @@ class Input extends Component {
   render() {
     return this.props.success ? null : (
       <form className="inputArea" data-test="component-input">
-        <input className="inputBox" data-test="component-input-box"></input>
+        <input
+          className="inputBox"
+          type="text"
+          placeholder="Enter your guess"
+          data-test="component-input-box"
+        ></input>
         <button
           type="submit"
           className="inputButton"
           data-test="component-submit-button"
-        ></button>
+        >
+          Submit
+        </button>
       </form>
     );
   }
