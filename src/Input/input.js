a5221ec0c08f3cb4ac266beb772a6a064a32411b
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-01 19:55:24 -0300

    input component and tests for success=false

--- src/Input/input.js
+++ src/Input/input.js
@@ -3,12 +3,21 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <section className="inputArea"></section>;
+    return this.props.success ? null : (
+      <form className="inputArea" data-test="component-input">
+        <input className="inputBox" data-test="component-input-box"></input>
+        <button
+          type="submit"
+          className="inputButton"
+          data-test="component-submit-button"
+        ></button>
+      </form>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
--- src/Input/input.test.js
+++ src/Input/input.test.js
@@ -13,12 +13,30 @@ const setup = (initialState = {}) => {
 };
 
 describe("render", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const initialState = { success: false };
+    wrapper = setup(initialState);
+  });
+
   describe("the word has not been guessed", () => {
-    it("renders the component without errors", () => {});
+    it("renders the component without errors", () => {
+      const inputComponent = wrapper.find("[data-test='component-input']");
+      expect(inputComponent.length).toBe(1);
+    });
 
-    it("renders the input box", () => {});
+    it("renders the input box", () => {
+      const inputBox = wrapper.find("[data-test='component-input-box']");
+      expect(inputBox.length).toBe(1);
+    });
 
-    it("renders the submit button", () => {});
+    it("renders the submit button", () => {
+      const submitButton = wrapper.find(
+        "[data-test='component-submit-button']"
+      );
+      expect(submitButton.length).toBe(1);
+    });
   });
 
   describe("the word has been guessed", () => {
