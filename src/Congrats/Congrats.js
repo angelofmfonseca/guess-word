a05a3626de8a784953f01576b67cb6c11ec431dc
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-01 19:58:21 -0300

    congrats component refactoring

--- src/Congrats/Congrats.js
+++ src/Congrats/Congrats.js
@@ -4,16 +4,15 @@ import PropTypes from "prop-types";
 import "./Congrats.css";
 
 export default function Congrats(props) {
-  if (props.success) {
-    return (
-      <section className="Congrats" data-test="congrats-component">
-        <span data-test="congrats-message">
-          Congratulations!!! You guessed the word!!!
-        </span>
-      </section>
-    );
-  }
-  return <section data-test="congrats-component" />;
+  return props.success ? (
+    <section className="Congrats" data-test="congrats-component">
+      <span data-test="congrats-message">
+        Congratulations!!! You guessed the word!!!
+      </span>
+    </section>
+  ) : (
+    <section data-test="congrats-component" />
+  );
 }
 
 Congrats.propTypes = {
