a833067ab00a1b86610e67292b64b7974081124b
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-09 18:39:05 -0300

    integration test dispatch guessWord for unsuccessfulGuess

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,8 +2,32 @@ import { storeFactory } from "./utils/testUtils";
 import { guessWord } from "./actions";
 
 describe("guessWord action dispatcher", () => {
+  const secretWord = "party";
+  const unsuccessfulGuess = "train";
+
   describe("no guessed words", () => {
-    it("updates state correctly for unsuccessful guess", () => {});
+    let store;
+    const initialState = { secretWord };
+
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+
+    it("updates state correctly for unsuccessful guess", () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          {
+            guessedWords: unsuccessfulGuess,
+            letterMatchCount: 3,
+          },
+        ],
+      };
+      expectedState(newState).toEqual(expectedState);
+    });
 
     it("updates state correctly for successful guess ", () => {});
   });
