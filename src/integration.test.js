db96745c0daed57de5e483bcb95026bb937d1565
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-09 00:47:53 -0300

    guessWords test setup

---
+++ src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from "./utils/testUtils";
+import { guessWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    it("updates state correctly for unsuccessful guess", () => {});
+
+    it("updates state correctly for successful guess ", () => {});
+  });
+
+  describe("some guessed word", () => {
+    it("updates state correctly for unsuccessful guess", () => {});
+
+    it("updates state correctly for successful guess", () => {});
+  });
+});
