c153bf79515384862d44fdc4bb9fc9c306ee3b29
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-09 00:17:47 -0300

    Redux Thunk installed + configStore e middlewares

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../reducers/index";
+import { middlewares } from "../configStore";
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
