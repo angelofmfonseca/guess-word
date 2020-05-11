17d51052b20b60d79b534571ffc69132e0bf6f9d
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-10 23:47:29 -0300

    setup secretWord action creator ans tests

---
+++ src/actions/actions.test.js
@@ -0,0 +1,12 @@
+import moxios from "moxios";
+
+describe("getSecretWord action creator", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it("adds response word to state", () => {});
+});
