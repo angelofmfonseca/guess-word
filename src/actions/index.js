2bab19aaa8453f21e258b05cc27dd4f190edd2be
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-10 23:45:37 -0300

    setup secretWord action creator and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import { getLetterMatchCount } from "../helpers";
+import axios from "axios";
 
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD",
+  SER_SECRET_WORD: "SET_SECRET_WORD",
 };
 
 export const guessWord = (guessedWord) => {
@@ -21,5 +23,11 @@ export const guessWord = (guessedWord) => {
     if (guessedWord === secretWord) {
       dispatch({ type: actionTypes.CORRECT_GUESS });
     }
+  };
+};
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get("http://localhost:3030");
   };
 };
